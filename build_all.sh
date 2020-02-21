a10f7444b5059448d1f9123e55b56ef8bc85a6de
mbello <mbello@users.noreply.github.com>
2020-02-21 18:32:01 -0300

    IMproved Dockerfiles

--- build_all.sh
+++ build_all.sh
@@ -2,18 +2,7 @@
 
 set -xe
 
-TAG=python3.8-minimal
-TAG_REPO=mpbello/"${TAG}":latest
-docker build --pull --no-cache --tag "${TAG_REPO}" https://github.com/mbello/dockerhub-mpbello-repository-images.git#:"${TAG}"
-docker push "${TAG_REPO}"
-docker image rm "${TAG_REPO}"
+./build_img.sh python3.8-minimal
+./build_img.sh python3.8-minimal-dev
 
-TAG=python3.8-minimal-dev
-TAG_REPO=mpbello/"${TAG}":latest
-docker build --pull --no-cache --tag "${TAG_REPO}" https://github.com/mbello/dockerhub-mpbello-repository-images.git#:"${TAG}"
-docker push "${TAG_REPO}"
-docker image rm "${TAG_REPO}"
-
-
-
-docker image prune
+# docker image prune
---
+++ build_img.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -xe
+
+if [ "$1" != "" ]; then
+    TAG="$1"
+    TAG_REPO=mpbello/"${TAG}":latest
+    docker build --pull --no-cache --tag "${TAG_REPO}" https://github.com/mbello/dockerhub-mpbello-repository-images.git#:"${TAG}"
+    docker push "${TAG_REPO}"
+else
+    echo "Please supply the name of an image"
+fi
+

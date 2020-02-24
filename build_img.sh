d6e3f19411417be2c35eb447328d82ac96ad9add
mbello <mbello@users.noreply.github.com>
2020-02-23 21:18:15 -0300

    Add python3-wheel to python3.8-minimal-dev

--- build_img.sh
+++ build_img.sh
@@ -6,7 +6,7 @@ if [ "$1" != "" ]; then
     TAG="$1"
     TAG_REPO=mpbello/"${TAG}":latest
     docker build --no-cache --tag "${TAG_REPO}" https://github.com/mbello/dockerhub-mpbello-repository-images.git#:"${TAG}"
-#    docker push "${TAG_REPO}"
+    docker push "${TAG_REPO}"
 else
     echo "Please supply the name of an image"
 fi
